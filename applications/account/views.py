d22a31e6cc7ab819a99b4113b0123fd404e37d35
Sulaimanovuran <sulaimanovuran@gmail.com>
2022-07-15 20:27:01 +0600

    change password

--- applications/account/views.py
+++ applications/account/views.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from applications.account import serializers
+from applications.account.serializers import RegisterSerializer, LoginSerializer, ChangePasswordSerializer
+
+User = get_user_model()
+
+
+class RegisterApiView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            message = f'Вы успешно зарегестрированы. ' \
+                      f'Bам отправлено письмо с активацией'
+            return Response(message, status=201)
+
+
+class ActivationView(APIView):
+    def get(self, request, activation_code):
+        try:
+            user = User.objects.get(activation_code=activation_code)
+            user.is_active = True
+            user.activation_code = ''
+            user.save()
+            return Response({'msg': 'Успешно'}, status=200)
+        except User.DoesNotExist:
+            return Response({'msg': 'Неверный код'}, status=400)
+
+
+class LoginApiView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializers = ChangePasswordSerializer(data=request.data,
+                                               context={'request': request})
+
+        serializers.is_valid(raise_exception=True) # проверка на корректность введенных данных
+        serializers.set_new_password()
+        return Response('Succesfull updated password')
--- shop/settings.py
+++ shop/settings.py
@@ -36,11 +36,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'rest_framework.authtoken',
+
     # my apps
     'applications.account',
     'applications.cart',
     'applications.product',
+
 
 ]
 
@@ -128,3 +132,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.CustomUser'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') # 'lciwlddkgrgectmf'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER') # 'sulaimanovuran@gmail.com'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
